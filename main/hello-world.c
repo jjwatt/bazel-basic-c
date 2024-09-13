cf39e26b6b6b0c89238121b2c1e0a98a649dff76
Jesse Wattenbarger <jwattenb@redhat.com>
2024-09-13 17:14:17 -0400

    A working simple build of a C main with bazel
    Using extern "C" from C++

---
+++ main/hello-world.c
@@ -0,0 +1,13 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+  printf("%s\n", "Hello, World");
+  return 0;
+} 
+#ifdef __cplusplus
+}
+#endif
